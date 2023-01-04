6d4f3d4baf44d8c8ba12046e6dd437a2b1f1abb6
endyjams <112718430+endyjams@users.noreply.github.com>
2023-01-04 15:38:55 -0300

    feat: homeScreen food PageView

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,58 @@
+import 'package:e_commerce/home/food_page_body.dart';
+import 'package:e_commerce/utils/colors.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Brazil", color: AppColors.mainColor,),
+                      Row(
+                        children: [
+                          SmallText(text: "Aracaju", color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
